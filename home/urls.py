c924261cbb7475ef6b80230ca1e65894a4dae49a
Vincent74230 <vincent.nowak@hotmail.fr>
2020-12-08 12:32:12 +0100

    ADDED an error route to debug sentry

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,13 @@
 from django.urls import path
 from . import views
 
+def trigger_error(request):
+    division_by_zero = 1/0
+
+
 app_name = "home"
 urlpatterns = [
     path("", views.index, name="index"),
     path("mentions-legales", views.mentions_legales, name="mentions"),
+    path('sentry-debug', trigger_error),
 ]
