7eb3f249a2aff1480632467d9cae005023501009
Vincent74230 <vincent.nowak@hotmail.fr>
2021-02-09 08:20:14 +0100

    NOW sending emails in text mode, next step:sendng templates

--- search/management/commands/email.py
+++ search/management/commands/email.py
@@ -3,15 +3,26 @@ Custom django command, fetches customer favourites and send it by email
 """
 from django.core.management.base import BaseCommand, CommandError
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from ...models import Products
 
 
 class Command(
     BaseCommand
 ):  # Custom django command, fetches customer favourites and send it by email
     help = "Custom django command, fetches customer favourites and send it by email"
-
     def handle(self, *args, **options):
-        send_mail('Bonjour de Pur Beurre',
-            'Ceci est un message automatique, merci de na pas y répondre',
-            'vincent.nowak@hotmail.fr',
-            ['vincent.nowaczyk@protonmail.com'])
+        all_users=User.objects.all()
+        for user in all_users:
+            if user.email:
+                favourites=Products.objects.filter(favourites=user.id)
+                if favourites:
+                    send_mail('Bonjour de Pur Beurre',
+                        'Bonjour {},\nVoici vos produits favoris : {}\nRevenez nous voir très vite'.format(user.username,favourites),
+                        'vincent.nowak@hotmail.fr',
+                        [user.email])
+                else:
+                    send_mail('Bonjour de Pur Beurre',
+                        "Vous n'avez pas enregistré de produits favoris chez nous, c'est dommage c'est les soldes chez nous!!!",
+                        "vincent.nowak@hotmail.fr",
+                        [user.email])
